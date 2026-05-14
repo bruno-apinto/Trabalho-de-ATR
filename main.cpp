663696263227819b96c2dfc2e0eba1b597852f60
Bruno Araújo <brunoapinto@gmail.com>
2026-05-14 20:57:35 -0300

    adicionados os mutex dentro das tarefas

--- main.cpp
+++ main.cpp
@@ -26,22 +26,6 @@ bool c_automatico; //Comando para passar o robô para o modo automático (true).
 bool c_man; //Comando para passar o robô para o modo manual (true).
 int j_sp_velocidade; //Setpoint de velocidade do robô para o controlador de velocidade.
 
-/* comandos sem definicao dada
-c_direita -
-Comando para acelerar o robô para a
-direita.
-
-c_esquerda -
-Comando para acelerar o robô para a
-esquerda.
-c_para - Comando para parar o robô.
-*/
-
-//variáveis de sincronização
-/* 
-std::condition_variable devagar;
-std::condition_variable camera; 
-*/
 
 /**
  * @brief Tarefa que recebe comandos do sistema de operação remoto e traduz os comandos em setpoint de velocidade
@@ -58,20 +42,24 @@ void comando_navegacao(){
  *  a partir de um setpoint de velocidade recebido pelo Comando de Navegação. Exerce a função de LEITOR do BUFFER _NAVEGACAO
  * 
  */
-void controle_navegacao(){
+void controle_navegacao(std::mutex mtx, std::vector <float> BUFFER){
+
+    std::unique_lock<std::mutex> lock (mtx);
+    lock.unlock();
 
 }
 
 void distancia_percorrida(std::mutex mtx, std::vector<float> BUFFER){
 
+    std::unique_lock<std::mutex> lock (mtx);
+    lock.unlock();
+
 }
 
 void reconstrucao_teto(std::mutex mtx, std::vector<float> BUFFER){
 
-    /* USAR SIGNAL AO INVES DE SEMAFORO
-    devagar.notify_all(); //avisa o robô que uma falha foi detectada
-    camera.notify_all(); //avisa a câmera que a falha foi detectada
-    */
+    std::unique_lock<std::mutex> lock (mtx);
+    lock.unlock();
 }
 
 void inspecao_camera(){
@@ -80,15 +68,13 @@ void inspecao_camera(){
 
 void coletor_dados(std::mutex mtx, std::vector<float> BUFFER){
 
+    std::unique_lock<std::mutex> lock (mtx);
+
+    lock.unlock();
 }
 
 void operacao_remota(std::mutex mtx, std::vector<float> BUFFER){
     std::unique_lock<std::mutex> lock(mtx); // Protocolo de entrada
-    /* USAR SIGNAL AO INVES DE SEMAFORO
-    while(!(e_inspecao)){
-        devagar.wait(lock); // Espera até que uma falha seja detectada
-    }
-    */
     lock.unlock(); // Protocolo de saída
 }
 
@@ -116,9 +102,7 @@ int main (){
 
    else {
 
-        comando_navegacao ();
-
-         //INICIALIZAÇÃO DE BUFFERS
+        //INICIALIZAÇÃO DE BUFFERS
         std::vector <float> BUFFER_NAVEGACAO (ELEMENTOS_BUFFERS); //posição do carrinho
         std::vector <float> BUFFER_NIVEL (ELEMENTOS_BUFFERS); //leitura de nivel
 
@@ -132,6 +116,11 @@ int main (){
         threads_navegacao.emplace_back(inspecao_camera);
         threads_navegacao.emplace_back(coletor_dados, mutex_nivel, BUFFER_NIVEL);
         threads_navegacao.emplace_back(reconstrucao_teto, mutex_nivel, BUFFER_NIVEL);
+        
+        
+        comando_navegacao ();
+
+
 
    }
 
