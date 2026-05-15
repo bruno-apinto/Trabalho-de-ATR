c50ab2f85ea92a478470826c8d2f85ddf2b4abf5
Bruno Araújo <brunoapinto2004@gmail.com>
2026-05-15 14:24:30 -0300

    Sincronizando threads controle_nav e dist_percorrida

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 #include <atomic>
 #include <stdlib.h>
 #include <chrono>
-#include <pthread.h>
 #include <condition_variable>
 #include <mutex>
 #include <semaphore>
 #include <boost/asio.hpp>
+#include <random>
 
 #define ELEMENTOS_BUFFERS 10
 
@@ -27,6 +27,11 @@ bool c_man; //Comando para passar o robô para o modo manual (true).
 int j_sp_velocidade; //Setpoint de velocidade do robô para o controlador de velocidade.
 
 
+//variaveis de condição buffers
+std::condition_variable leitura_buffer_navegacao;
+int dados_fila = 0;
+std::condition_variable escrita_buffer_navegacao;
+
 /**
  * @brief Tarefa que recebe comandos do sistema de operação remoto e traduz os comandos em setpoint de velocidade
  *  e liga/desliga para o controle de navegação. O comando de navegação que deve implementar a lógica de manual/ 
@@ -42,21 +47,57 @@ void comando_navegacao(){
  *  a partir de um setpoint de velocidade recebido pelo Comando de Navegação. Exerce a função de LEITOR do BUFFER _NAVEGACAO
  * 
  */
-void controle_navegacao(std::mutex &mtx, std::vector <float> *BUFFER){
+void controle_navegacao(std::mutex &mtx, std::vector <float> &BUFFER){
 
-    std::unique_lock<std::mutex> lock (mtx);
-    lock.unlock();
+    int idx = 0;
+
+    for (int i = 0; i < 13; i++){
+        
+        idx++;
+        idx = idx%ELEMENTOS_BUFFERS;
+        
+        std::unique_lock<std::mutex> lock (mtx);
+
+        while(dados_fila == 0){
+            leitura_buffer_navegacao.wait(lock);
+        }
+
+        std::cout << "Posição lida: " << BUFFER[idx] << std::endl;
+        lock.unlock();
+        dados_fila--;
+        escrita_buffer_navegacao.notify_one();
+
+    }
 
 }
 
-void distancia_percorrida(std::mutex mtx, std::vector<float> *BUFFER){
+void distancia_percorrida(std::mutex &mtx, std::vector <float> &BUFFER){
+    int idx = 0;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(1.0f, 100.0f);
 
-    std::unique_lock<std::mutex> lock (mtx);
-    lock.unlock();
+    for (int i = 0; i < 13; i++){
+        idx++;
+        idx = idx % ELEMENTOS_BUFFERS;
+        float random_num = dis(gen);
+    
+        std::unique_lock<std::mutex> lock (mtx);
+        
+        while(dados_fila >=10){
+            escrita_buffer_navegacao.wait(lock);
+        }
+        BUFFER[idx] = random_num;
+        lock.unlock();
 
+        dados_fila++;
+        leitura_buffer_navegacao.notify_one();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200)); // Ajustado tempo para testes rápidos
+    }
 }
 
-void reconstrucao_teto(std::mutex mtx, std::vector<float> *BUFFER){
+void reconstrucao_teto(std::mutex &mtx, std::vector <float> &BUFFER){
 
     std::unique_lock<std::mutex> lock (mtx);
     lock.unlock();
@@ -66,14 +107,14 @@ void inspecao_camera(){
 
 }
 
-void coletor_dados(std::mutex mtx, std::vector<float> *BUFFER){
+void coletor_dados(std::mutex &mtx, std::vector <float> &BUFFER){
 
     std::unique_lock<std::mutex> lock (mtx);
 
     lock.unlock();
 }
 
-void operacao_remota(std::mutex mtx, std::vector<float> *BUFFER){
+void operacao_remota(std::mutex &mtx, std::vector <float> &BUFFER){
     std::unique_lock<std::mutex> lock(mtx); // Protocolo de entrada
     lock.unlock(); // Protocolo de saída
 }
@@ -112,12 +153,26 @@ int main (){
         std::mutex mutex_nivel;
 
         std::vector <std::thread> threads_navegacao;
-        threads_navegacao.emplace_back(distancia_percorrida, std::ref (mutex_navegacao), &BUFFER_NAVEGACAO);
+        threads_navegacao.emplace_back(distancia_percorrida, std::ref (mutex_navegacao), std::ref(BUFFER_NAVEGACAO));
         threads_navegacao.emplace_back(inspecao_camera);
-        threads_navegacao.emplace_back(coletor_dados, std::ref (mutex_nivel), &BUFFER_NIVEL);
-        threads_navegacao.emplace_back(reconstrucao_teto, std::ref (mutex_nivel), &BUFFER_NIVEL);
+        threads_navegacao.emplace_back(coletor_dados, std::ref (mutex_nivel), std::ref(BUFFER_NIVEL));
+        threads_navegacao.emplace_back(reconstrucao_teto, std::ref (mutex_nivel), std::ref(BUFFER_NIVEL));
+
+        controle_navegacao(std::ref (mutex_navegacao), std::ref(BUFFER_NAVEGACAO));
+
+        for (int i = 0; i < threads_navegacao.size(); i++){
+            threads_navegacao[i].detach();
+        }
         
-        controle_navegacao(std::ref (mutex_navegacao), &BUFFER_NAVEGACAO);
+
+
+        std::cout << "Buffer navegação: ";
+
+        for (auto i : BUFFER_NAVEGACAO){
+            std::cout << i << " ";
+        }
+
+        std::cout << std::endl;
    }
 
     return 0;
